29f49b692222d419c2d298760b55d47f6df93553
dpalexandru <alexpoian91@hotmail.com>
2025-09-09 15:32:49 +0200

    feat: create new budget context

---
+++ src/context/BudgetContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+
+// creo context
+const BudgetContext = createContext();
+
+const BudgetProvider = ({ children }) => {
+  const [budgetMode, setBudgetMode] = useState(false);
+
+  // funzione  per modificare lo stato
+  const toggleBudgetMode = () => {
+    setBudgetMode(prev => !prev);
+  };
+
+  const value = {
+    budgetMode,
+    toggleBudgetMode,
+  };
+
+  return (
+    <BudgetContext.Provider value={value}>
+      {children}
+    </BudgetContext.Provider>
+  );
+};
+
+// esporto provider e context 
+export { BudgetProvider, BudgetContext };

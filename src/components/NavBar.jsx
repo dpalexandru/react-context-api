2c6c66dddf592511a9d58f5a9eaab8d29ac30375
dpalexandru <alexpoian91@hotmail.com>
2025-09-09 16:02:27 +0200

    feat: add budget context in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,36 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useContext } from 'react'
+import { BudgetContext } from '../context/BudgetContext'
 
 const Navbar = () => {
+
+  //destructoring BudgetContext
+  const { budgetMode, toggleBudgetMode } = useContext(BudgetContext);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
+        {/* logo */}
+
         <NavLink className="navbar-brand fw-bold" to="/">
           Fake Store
         </NavLink>
 
-        {/* Bottone modalità budget */}
+        {/* bottone modalità budget */}
         <div className="mx-auto">
-          <button className="btn btn-outline-warning">
+          <button className={`btn ${budgetMode ? "btn-warning" : "btn-outline-warning"}`}
+            onClick={() => {
+              toggleBudgetMode();
+              console.log(budgetMode)
+            }}
+          >
             Imposta budget
             <i className="bi bi-cash-stack m-2"></i>
           </button>
         </div>
 
+        {/* menu navigazione */}
         <ul className="navbar-nav ms-auto  ">
           <li className="nav-item ">
             <NavLink className="nav-link" to="/">
